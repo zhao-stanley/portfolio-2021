331f24710d13e850b2cf791d3d26df6a08deabb6
zhao-stanley <contact@stanleyweb.dev>
2022-06-07 01:22:28 -0400

    Navbar tweak

--- src/components/Global/Navbar.tsx
+++ src/components/Global/Navbar.tsx
@@ -5,7 +5,7 @@ import { github } from "./DarkModeIcons";
 export const Navbar = () => {
   return (
     <>
-      <nav className="fixed w-full h-min p-4 max-w-[82rem] flex flex-row justify-between items-center backdrop-filter backdrop-blur-md bg-opacity-30 firefox:bg-opacity-90 z-50">
+      <nav className="fixed w-full h-min p-4 max-w-[88rem] flex flex-row justify-between items-center backdrop-filter backdrop-blur-md bg-opacity-30 firefox:bg-opacity-90 z-50">
         <Logo />
         <div className="flex flex-row items-center gap-4">
           <DarkModeToggle />
