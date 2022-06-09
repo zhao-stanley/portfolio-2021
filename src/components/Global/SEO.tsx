514ec69833c53c7e01ca441f320b1f748ed88d5b
zhao-stanley <contact@stanleyweb.dev>
2022-06-09 19:22:42 -0400

    SEO Update

---
+++ src/components/Global/SEO.tsx
@@ -0,0 +1,57 @@
+import Head from "next/head";
+
+export const SEO = () => {
+  return (
+    <>
+      <Head>
+        <title>Stanley Zhao</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/img/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/img/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/img/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/img/site.webmanifest" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="title" content="Stanley Zhao" />
+        <meta
+          name="description"
+          content="Hey there! I'm a junior in high school who loves creating beautiful and functional web applications. I'm currently a web developer at the Competitive Programming Initiative, which promotes competitive programming through the USACO Guide. I also do competitive programming and cybersecurity on the side. Check out some of my projects or view my GitHub profile @zhao-stanley."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.szhao.dev/" />
+        <meta property="og:title" content="Stanley Zhao" />
+        <meta
+          property="og:description"
+          content="Hey there! I'm a junior in high school who loves creating beautiful and functional web applications. I'm currently a web developer at the Competitive Programming Initiative, which promotes competitive programming through the USACO Guide. I also do competitive programming and cybersecurity on the side. Check out some of my projects or view my GitHub profile @zhao-stanley."
+        />
+        <meta
+          property="og:image"
+          content="https://www.szhao.dev/img/preview.png"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.szhao.dev/" />
+        <meta property="twitter:title" content="Stanley Zhao" />
+        <meta
+          property="twitter:description"
+          content="Hey there! I'm a junior in high school who loves creating beautiful and functional web applications. I'm currently a web developer at the Competitive Programming Initiative, which promotes competitive programming through the USACO Guide. I also do competitive programming and cybersecurity on the side. Check out some of my projects or view my GitHub profile @zhao-stanley."
+        />
+        <meta
+          property="twitter:image"
+          content="https://www.szhao.dev/img/preview.png"
+        />
+      </Head>
+    </>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { SEO } from "../components/Global/SEO";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <SEO/>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
