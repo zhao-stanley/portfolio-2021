836c261a2dafe8b1f784a99aa032c605f19d58e4
zhao-stanley <contact@stanleyweb.dev>
2022-07-06 17:58:54 -0400

    Experience update

--- src/components/home/experience/Experience.tsx
+++ src/components/home/experience/Experience.tsx
@@ -1,22 +1,36 @@
 import { motion } from "framer-motion";
 import experience from "./experience.json";
-import { SchoolIcon, JobIcon, InternshipIcon } from "./ExpIcons";
+import { JobIcon, InternshipIcon } from "./ExpIcons";
 
 const expList = experience.map((e, key) => {
-  return (
+  return e.href ? (
+    <a href={e.href} key={key} target="_blank">
+      <motion.li
+        whileHover={{ scale: 1.01 }}
+        whileTap={{ scale: 0.99 }}
+        className="flex flex-row gap-3 xl:gap-4 shadow-md items-center justify-start bg-slate-200 dark:bg-[#2b2929] w-[90vw] sm:w-[75vw] xl:w-[65vw] max-w-5xl text-black dark:text-slate-200 px-4 py-4 rounded-xl"
+      >
+        {e.type == "internship" ? <InternshipIcon /> : <JobIcon />}
+        <div className="flex flex-col">
+          <h1 className="text-xl xl:text-2xl text-black dark:text-white font-semibold">
+            {e.title}
+          </h1>
+          <h2 className="text-md xl:text-lg text-gray-700 dark:text-gray-300 font-normal">
+            <span className="font-semibold">{e.location}</span>
+            <br />
+            {e.tenure}
+          </h2>
+        </div>
+      </motion.li>
+    </a>
+  ) : (
     <motion.li
       key={key}
       whileHover={{ scale: 1.01 }}
       whileTap={{ scale: 0.99 }}
       className="flex flex-row gap-3 xl:gap-4 shadow-md items-center justify-start bg-slate-200 dark:bg-[#2b2929] w-[90vw] sm:w-[75vw] xl:w-[65vw] max-w-5xl text-black dark:text-slate-200 px-4 py-4 rounded-xl"
     >
-      {e.type == "school" ? (
-        <SchoolIcon />
-      ) : e.type == "internship" ? (
-        <InternshipIcon />
-      ) : (
-        <JobIcon />
-      )}
+      {e.type == "internship" ? <InternshipIcon /> : <JobIcon />}
       <div className="flex flex-col">
         <h1 className="text-xl xl:text-2xl text-black dark:text-white font-semibold">
           {e.title}
--- src/components/home/projects/Projects.tsx
+++ src/components/home/projects/Projects.tsx
@@ -2,14 +2,6 @@ import { motion } from "framer-motion";
 import projects from "./projects.json";
 import { Tags } from "./Tags";
 
-const videos = [
-  "/videos/ubg.mp4",
-  "/videos/parallax.mp4",
-  "/videos/jcp.mp4",
-  "/videos/sticker.mp4",
-  "/videos/platform.mp4",
-];
-
 const projList = projects.map((e, index) => {
   let tagsArr = e.tags;
   return (
@@ -20,48 +12,47 @@ const projList = projects.map((e, index) => {
       className="rounded-xl p-4 xl:p-5 drop-shadow-xl flex flex-col justify-between"
       style={{ backgroundColor: `#${e.theme}` }}
     >
-      <div>
-        <video
-          src={videos[index]}
-          placeholder="blur"
-          className="rounded-md shadow-md shadow-black"
-          autoPlay
-          muted
-          loop
-          disablePictureInPicture
-          playsInline
-        ></video>
-        <div className="my-4">
-          <a
-            href={e.link}
-            className="flex items-center gap-1 w-min"
-            target="_blank"
-            rel="noreferrer"
+      <video
+        src={`/videos/${e.video}`}
+        placeholder="blur"
+        className="rounded-md shadow-md shadow-black"
+        autoPlay
+        muted
+        loop
+        disablePictureInPicture
+        playsInline
+      ></video>
+      <div className="my-4">
+        <a
+          href={e.link}
+          className="flex items-center gap-1 w-min"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h1 className="text-2xl xl:text-3xl font-semibold text-white whitespace-nowrap">
+            {e.name}
+          </h1>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-auto w-4 xl:w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="#fff"
+            strokeWidth={1}
           >
-            <h1 className="text-2xl xl:text-3xl font-semibold text-white whitespace-nowrap">
-              {e.name}
-            </h1>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-auto w-4 xl:w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="#fff"
-              strokeWidth={1}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-              />
-            </svg>
-          </a>
-          <hr className="my-1 xl:my-2" />
-          <h2 className="text-xs xl:text-sm font-normal text-gray-100">
-            {e.desc}
-          </h2>
-        </div>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            />
+          </svg>
+        </a>
+        <hr className="my-1 xl:my-2" />
+        <h2 className="text-xs xl:text-sm font-normal text-gray-100">
+          {e.desc}
+        </h2>
       </div>
+
       <Tags tagsArr={tagsArr} />
     </motion.li>
   );
